d96f740bc9d03a0d3c93ff6e13df708753df981b
iamvishaldev <vishalyadavdev1@example.com>
2025-07-10 10:52:47 +0530

    Re Structure the folder

---
+++ Day1.js
@@ -0,0 +1,48 @@
+console.log("Hello, DSA Journey!");
+
+// Data types in JavaScript
+let number = 42; // Number
+let string = "Hello, World!"; // String
+let boolean = true; // Boolean
+let array = [1, 2, 3, 4, 5]; // Array
+let object = { key: "value" }; // Object
+let nullValue = null; // Null
+let undefinedValue; // Undefined
+
+
+const a = 10; // Constant variable
+const b = 20; // Another constant variable
+// Arithmetic operations
+const sumResult = a + b; // Sum of a and b
+
+const multiplyResult = a * b; // Multiply a and b
+const differenceResult = a - b; // Difference of a and b
+
+// This is how we store value in a variable
+
+//console.log("Multiply of a and b is:", multiplyResult); // Output the result
+//console.log("Sum of a and b is:", sumResult); // Output the result
+//console.log("Difference of a and b is:", differenceResult); // Output the result
+
+
+let x = 10
+
+x = 20; // This will throw an error because x is a constant
+
+console.log("Value of x is:", x); // This line will not execute due to the error above
+
+let firstName = 1; // String variable
+let lastName = 7; // Another string variable
+
+firstName = "Vishal"; // Reassigning firstName to a string
+lastName = " Yadav"; // Reassigning lastName to a string
+
+// Concatenating strings
+let fullName = firstName + lastName; // Concatenation using the plus operator
+console.log("Full Name:", fullName); // Output the full name
+
+let numArr = [1, 2, 3, 4, 5]; // Array declaration
+console.log("Array:", numArr[6]); // Output the array
+
+let strArr = ["Hello", "World", "!", true,"xyz",false,[5,10]]; // String array declaration
+console.log("String Array:", strArr[6][0]); // Output the first element of the string
