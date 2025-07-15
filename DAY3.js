f051307fd07d77574acff8ea625a586ca9c38a4a
iamvishaldev <vishalyadavdev1@example.com>
2025-07-15 15:45:19 +0530

    Day 4 completed

---
+++ DAY3.js
@@ -0,0 +1,78 @@
+// loops
+// for(let i =0;i<=4;i++){
+// console.log("Hello world" + i)
+// }
+
+// for(let i =3;i<5;i=i+1){
+// console.log(i + " " + "Hello world")
+// }
+
+// for(let i =1;i<=5;i++){
+// console.log(i + " " +"Hello world")
+// }
+
+// for(let i=0;i<=4;i=i+1){
+//     console.log(i + " " + "Hello world")
+// }
+
+// for(let i=2;i<9;i=i+2){
+// console.log("Hello world" + " " + i)
+// }
+
+// for(let i =2;i<9;i=i+3){
+// console.log("HW")
+// }
+
+// Reverse loop
+
+// for(let i =0;i>0;i++){
+// console.log("Hello world", i)
+// }
+
+// function greet(i){
+//         console.log("hw" + " " + i)
+
+// }
+
+// for(let i = 10;i>=0;i--){
+//     greet(i)
+// }
+
+// Reverse Loop
+
+// for(let i=5;i>0;i=i-1){
+//     console.log("HW", + " " + i)
+// }
+
+// for(let i =3;i<5;i++){
+// console.log("Hello world",i)
+// }
+
+// reverse loop
+
+// for(let i =5;i>=0;i=i-1){
+// console.log("HW",i)
+// }
+
+// let arr = [2,4,6,8,9,1]
+
+// for(let i=0;i<arr.length;i++){
+// console.log("Hello world",arr[i])
+// }
+
+// Print all the even numbers in the array
+
+// let arr = [10,20,6,2,4,6,7,8,9,1,5]
+
+// for(let i =0;i<arr.length;i++){
+//     if(arr[i]%2 === 0){
+//         console.log("hw",arr[i])
+//     }
+// }
+
+// while loop
+let i =0
+while(i<5){
+    console.log(i);
+    i++;
+}
