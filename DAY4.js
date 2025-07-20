f10677f8b8874343465ea284457066c1277935d3
iamvishaldev <vishalyadavdev1@example.com>
2025-07-20 11:50:15 +0530

    Day4 completed

---
+++ DAY4.js
@@ -0,0 +1,107 @@
+// Q1 --->
+
+// Write a funtion that searches for an element in an array and return the index, if the element is not present then return -1.
+
+// function searchElement(arr,x){
+//     for(let i=0;i<arr.length;i++){
+//        if(arr[i] === x){
+//         return i
+//        }
+//     }
+//     return -1
+// }
+
+
+// let arr =[4,2,0,10,8,30]
+// console.log(searchElement(arr,30))
+
+
+// Q2 --->
+// Write a function that return the number of negative numbers in an array
+
+// function checkNegative(arr){
+//     let newArr =[]
+//     for(let i =0;i<arr.length;i++){
+//         if(arr[i] <0){
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr.length
+// }
+// let arr =[2,-9,17,0,1,-10,-4,8]
+
+// console.log(checkNegative(arr))
+
+// Count even numbers
+
+// function checkEvenNumber(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]%2 === 0){
+//             count ++
+//         }
+//     }
+//     return count
+// }
+
+// let arr =[2,3,4,8,9,10]
+// console.log(checkEvenNumber(arr))
+
+
+// function checkEvenNumber(arr){
+//     let evenArray = []
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]% 2 === 0){
+//             evenArray.push(arr[i])
+//         }
+//     }
+//     return evenArray.length;
+// }
+// let arr =[2,-9,17,0,1,-10,-4,8]
+// console.log(checkEvenNumber(arr))
+
+// Q4 --->
+// Write a function that returns the count of even numbers in an array.
+
+// function checkEvenNumber(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]%2 === 0){
+//             count ++
+//         }
+//     }
+//     return count
+// }
+
+// let arr =[2,3,4,8,9,10]
+// console.log(checkEvenNumber(arr))
+
+
+// function checkEvenNumber(arr){
+//     let evenArray = []
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]% 2 === 0){
+//             evenArray.push(arr[i])
+//         }
+//     }
+//     return evenArray.length;
+// }
+// let arr =[2,-9,17,0,1,-10,-4,8]
+// console.log(checkEvenNumber(arr))
+
+// Q5 --->
+// Write a function that returns the total sum of elements in an array.
+
+    // function sumOfAll(arr){
+    //     let a = arr[0]
+    //     for(let i = 0;i<arr.length;i++){
+
+    //     }
+    // }
+    // let arr = [1,2,3,4,5]
+    // sumOfAll(arr)
+// Q6 --->
+// Write a function that reverses the elements of an array without using .reverse()
+
+// Q7 --->
+// Write a function that returns a new array of only the positive numbers.
