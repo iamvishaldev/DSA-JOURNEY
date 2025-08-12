beccc683daee972bf178a52178cb391c5f8ba18c
iamvishaldev <vishalyadavdev1@example.com>
2025-08-12 11:01:14 +0530

    Patterns

---
+++ pattern.js
@@ -0,0 +1,60 @@
+// for(let i=0;i<4;i++){
+//     for(let j=0;j<4;j++){
+//         console.log("i",i,"j",j)
+//     }
+// }
+
+// Square Pattern
+
+// let n=4;
+// for(let i=0;i<n;i++){
+//     let row = "";
+//     for(let j =0;j<n;j++){
+//         row = row + "* "
+//     }
+//     console.log(row)
+// }
+
+// Right-Angled Traingle
+
+// let n = 4;
+// for(let i=0;i<n;i++){
+//     let row = " "
+//     for(let j=0;j<i+1;j++){
+//         row= row + "* "
+//     }
+//     console.log(row)
+// }
+
+// Right-Angled Traingle
+
+// let n=5;
+
+// for(let i=0;i<n;i++){
+//     let row = ""
+//     for(let j=0;j<=i;j++){
+//         row = row + "*"
+//     }
+//     console.log(row)
+// }
+
+// output:-
+// *
+// **
+// ***
+// ****
+// *****
+
+// Numbers Right-Angled Traingle
+
+// let n =4;
+
+// for(let i=0;i<n;i++){
+//     let row="";
+//     for(let j=0;j<=i;j++){
+//        row = row + (j+1)
+//     }
+//     console.log(row)
+// }
+
+// Inverted Right-Angled Triangle
